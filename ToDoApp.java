bfc449e5a86e97cf18cf28cd3afa651e0afb9af2
Dwight Dwayne Yu <yudwightdwayne@gmail.com>
2024-12-14 21:01:27 +0800

    Initial commit

---
+++ ToDoApp.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ToDoApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //TO DO: Implement main loop and other features. The main loop should be responsible for asking user whether or not to continue using the app.
+    }
+}
