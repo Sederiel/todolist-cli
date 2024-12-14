c40946e1b0b40b72a1668e6550884a78ed520b25
Sederiel <sederiel0100@gmail.com>
2024-12-14 21:14:31 +0800

    Update ToDoApp.java

--- ToDoApp.java
+++ ToDoApp.java
@@ -6,8 +6,13 @@ public class ToDoApp {
 
         char option;
         do {
+            System.out.println("==============");
+            System.out.println("| TO-DO LIST |");
+            System.out.println("==============");
+            
             System.out.print("Do you wish to input again? [Y/N]: ");
             option = input.next().charAt(0);
+            System.out.println();
         } while(option == 'y' || option == 'Y');
     }
 }
